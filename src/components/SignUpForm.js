76af029a4ae5160992b573e0227807674681c8fd
dan muendo <danwalker254254@gmail.com>
2022-12-08 16:55:54 +0000

    first commit

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { Button, Error, Input, FormField, Label, Textarea } from "../styles";
+
+function SignUpForm({ onLogin }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+    // console.log(firstName, lastName, email, gender, age, password, passwordConfirmation);
+    fetch("http://127.0.0.1:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        gender: gender,
+        age: age,
+        password: password,
+        password_confirmation: passwordConfirmation,
+
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormField>
+        <Label htmlFor="username">First Name</Label>
+        <Input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="lastName">Last Name</Label>
+        <Input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          autoComplete="current-password"
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="email">Email</Label>
+        <Input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoComplete="current-password"
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="gender">Gender</Label>
+        <Input
+          type="text"
+          id="gender"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          autoComplete="current-password"
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="age">Age</Label>
+        <Input
+          type="number"
+          id="age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          autoComplete="current-password"
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="password">Password Confirmation</Label>
+        <Input
+          type="password"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          autoComplete="current-password"
+        />
+      </FormField>
+      
+  
+      <FormField>
+        <Button type="submit">{isLoading ? "Loading..." : "Sign Up"}</Button>
+      </FormField>
+      <FormField>
+        {errors.map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+      </FormField>
+    </form>
+  );
+}
+
+export default SignUpForm;
