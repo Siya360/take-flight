f264e2ea483ff23fc0fe653bbb27b4887191cf67
SiyaR <siya360@gmail.com>
2023-12-31 04:48:18 +0200

    Integrated NewFlight.js component with the API Gateway

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -12,3 +12,22 @@ export const fetchFlights = async () => {
     throw error; // Re-throw to handle it in the calling component
   }
 };
+
+export const createFlight = async (flightData) => {
+  try {
+    const response = await fetch(`${BASE_URL}/flights`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(flightData),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error creating new flight:', error);
+    throw error;
+  }
+};
