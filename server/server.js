d9ec81157556461296311114a7bc234e6f5e8eb0
SiyaR <siya360@gmail.com>
2024-01-08 16:02:31 +0200

    Migrated code to SDK for JavaScript V3

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const AWS = require('aws-sdk');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const { validateToken } = require('./authMiddleware');
